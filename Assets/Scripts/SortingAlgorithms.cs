1bea0c7621ee6f3a4daf9b7f096b3efed9351fa0
Arthur Ribeiro <arthhur.ribeiro@gmail.com>
2022-06-23 16:23:20 -0300

    Create SortingAlhorithm class

---
+++ Assets/Scripts/SortingAlgorithms.cs
@@ -0,0 +1,85 @@
+using System;
+using UnityEditor;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+namespace CircuitStream
+{
+    [InitializeOnLoad]
+    public static class SortingAlgorithms
+    {
+        private static readonly int[] arrayTestSizes = {0, 1, 5, 1000};
+        private static int currentTestSizeIndex = 2;
+        private static int CurrentTestSize => arrayTestSizes[currentTestSizeIndex];
+
+        [MenuItem("Sorting Algorithms/Change Test Size")]
+        private static void CycleTestSize()
+        {
+            currentTestSizeIndex = (currentTestSizeIndex + 1) % arrayTestSizes.Length;
+            Debug.Log($"New Test Size: {CurrentTestSize}");
+        }
+
+        [MenuItem("Sorting Algorithms/Insertion Sort")]
+        private static void InsertionSort()
+        {
+            var array = GenerateRandomArray();
+            Debug.LogWarning($"Starting Array: {String.Join(", ", array)}");
+
+            // TODO
+
+            Debug.Log($"Result: {String.Join(", ", array)}");
+        }
+
+        [MenuItem("Sorting Algorithms/Bubble Sort")]
+        private static void BubbleSort()
+        {
+            var array = GenerateRandomArray();
+            Debug.LogWarning($"Starting Array: {String.Join(", ", array)}");
+
+            int tmp;
+            bool arrayChanged;
+
+            for (int i = 0; i <= array.Length - 2; i++)
+            {
+                arrayChanged = false;
+                for (int j = 0; j <= array.Length - 2; j++)
+                {
+                    if (array[j] > array[j + 1])
+                    {
+                        tmp = array[j + 1];
+                        array[j + 1] = array[j];
+                        array[j] = tmp;
+
+                        arrayChanged = true;
+                    }
+                }
+
+                if (!arrayChanged) break;
+            }
+
+            Debug.Log($"Result: {String.Join(", ", array)}");
+        }
+
+        [MenuItem("Sorting Algorithms/Merge Sort")]
+        private static void MergeSort()
+        {
+            var array = GenerateRandomArray();
+            Debug.LogWarning($"Starting Array: {String.Join(", ", array)}");
+
+            // TODO
+
+            Debug.Log($"Result: {String.Join(", ", array)}");
+        }
+
+        private static int[] GenerateRandomArray()
+        {
+            var array = new int[CurrentTestSize];
+
+            for (int i = 0; i < CurrentTestSize; i++)
+                array[i] = Random.Range(-1000, 1000);
+
+            return array;
+        }
+    }
+}
+
